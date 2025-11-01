20593ff28b88edd91219601639d662a6059d2e96
Max Klenov <maxklenow@gmail.com>
2025-11-01 14:57:12 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import netlify from "@netlify/vite-plugin";
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), netlify(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
