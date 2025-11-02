c80a8232dc75a87e0bf76d3cbc56f675fbb8fb86
Max Klenov <maxklenow@gmail.com>
2025-11-02 13:28:19 +0300

    bugfix: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import netlify from "@netlify/vite-plugin";
-import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), netlify(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  plugins: [react(), netlify()]
 });
