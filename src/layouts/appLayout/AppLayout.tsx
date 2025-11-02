062c8990b3cf3e9be9a8f3c44760e05ef9b2b9b3
Max Klenov <maxklenow@gmail.com>
2025-11-02 13:26:04 +0300

    bugfix: alias

--- src/layouts/appLayout/AppLayout.tsx
+++ src/layouts/appLayout/AppLayout.tsx
@@ -1,4 +1,4 @@
-import Content from "../../components/content/Content";
+import Content from '@/components/content/Content';
 import styles from './appLayout.module.css'
 
 const AppLayout = () => {
