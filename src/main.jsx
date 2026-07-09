62a69b2f2e6ff01e6e156e4e654d602d551c3b30
Max Klenov <maxklenow@gmail.com>
2026-07-09 18:24:14 +0300

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './assets/styles/global.css'
 import AppLayout from './layout/AppLayout';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppLayout />
   </StrictMode>,
