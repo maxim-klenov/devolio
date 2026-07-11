8da485fa8ba7e7a2d3d5a9320cf452fadd6b7945
Max Klenov <maxklenow@gmail.com>
2026-07-11 16:09:31 +0300

    intro

--- src/components/sections/heroSection/HeroSection.jsx
+++ src/components/sections/heroSection/HeroSection.jsx
@@ -8,17 +8,21 @@ import heroJpg from '@/assets/images/hero-image.jpg';
 const HeroSection = () => {
   return ( 
     <>
-      <div className={styles.intro}> 
-        <sup className={styles.suptext}>Привет, привет!</sup>
-        <h1 className={styles.heading}>Это - кратенькое портфолио Jamstack-разработчика</h1>
+      <section aria-labelledby="heading-intro"> 
+        <div className={styles.intro}>
+          <hgroup>
+            <sup className={styles.suptext}>Привет, привет!</sup>
+            <h1 className={styles.heading} id="heading-intro">Это - кратенькое <span>портфолио</span> <br/> Jamstack-разработчика</h1>
+          </hgroup>
 
-        <picture className={styles.hero}>
-          <source srcSet={heroWebp}></source>
-          <img src={heroJpg} alt="улыбающийся мальчик сидит за компьютером и машет рукой" width={388} height={388}/>
-        </picture>
-      </div>
-
-      <Icon name="arrow-down" width={100} height={100}/>
+          <picture className={styles.hero}>
+            <source srcSet={heroWebp}></source>
+            <img src={heroJpg} alt="улыбающийся мальчик сидит за компьютером и машет рукой" width={388} height={388}/>
+          </picture>
+        </div>
+        
+        <Icon className={styles["arrow-down"]} name="arrow-down" width={100} height={100}/>
+      </section>
     {/* <ScrollContainer /> */}
     </>
    );
