d10357e4886d0ae8c77b3986ca546b67e0c485b7
Max Klenov <maxklenow@gmail.com>
2026-07-11 13:08:29 +0300

    fixes

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,13 @@
 import styles from './appLayout.module.css'
 import Header from '@/components/header/Header';
-// import Content from '@/components/navigation/content/Content';
+import Content from '@/components/content/Content';
 
 const AppLayout = () => {
   return ( 
     <div className={styles.layout}>
       {/* header content footer */}
       <Header />
-      {/* <Content /> */}
+      <Content />
     </div>
    );
 }
