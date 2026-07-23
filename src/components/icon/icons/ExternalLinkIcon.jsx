296b97eb1f0a444ae64aa4e62c850fa6efae566f
Max Klenov <maxklenow@gmail.com>
2026-07-23 19:19:41 +0300

    project: icons

---
+++ src/components/icon/icons/ExternalLinkIcon.jsx
@@ -0,0 +1,26 @@
+const ExternalLinkIcon = ({ color, width, height, ariaHidden, ariaLabel, ...props }) => {
+  return ( 
+    <svg
+      stroke={color}
+      stroke-width="2.5" 
+      stroke-linecap="round" 
+      stroke-linejoin="round"
+      width={width}
+      height={height}
+      aria-hidden={ariaHidden}
+      aria-label={ariaLabel}
+      fill="none"
+      viewBox="0 0 25 25"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+      >
+        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line>
+      </svg>
+
+   );
+}
+ 
+export default ExternalLinkIcon;
+
+
+// <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#00c7ff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"></svg>
