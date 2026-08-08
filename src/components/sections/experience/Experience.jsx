53fc110f8402cb611bc1b01705b170ab94d65997
Max Klenov <maxklenow@gmail.com>
2026-08-09 00:07:49 +0300

    exp: card structure

--- src/components/sections/experience/Experience.jsx
+++ src/components/sections/experience/Experience.jsx
@@ -1,27 +1,35 @@
 import clsx from "clsx"; // do you need that 
 import styles from "./experience.module.css";
 
+import Icon from "@/components/icon/Icon";
+import cardImage from "@/assets/images/work-card.jpg"
+
 const Experience = () => {
   return ( 
     <section id="exp-section" className={clsx("section", styles.exp)}>
       <h2 className="heading-2">Опыт и достижения</h2>
 
       <div className={styles.group}>
-        <p>
+        <p className={styles.para}>
           В веб разработкe я успел, и поучиться, и побыть в команде, и, конечно, набраться опыта. Где-то год я реализовывал себя в компании «преакс плэтформ» в качестве фронтенд-разработчика...
         </p>
-        <article>
+
+        <article className={styles.card}>
+
+          <img src={cardImage} alt="preax лого" width="46" height="46" />
+
           <sup>PREAX</sup>
           <span>Стажер frontend (React)</span>
           <sub>Март 2025 - 1 год и 2 месяца</sub>
+
           <ul className={styles["inner-stack"]}>
-              <li className={styles["stack-chip"]}>CSS</li>
-              <li className={styles["stack-chip"]}>TypeScript</li>
-              <li className={styles["stack-chip"]}>JavaScript</li>
-              <li className={styles["stack-chip"]}>Git</li>
-              <li className={styles["stack-chip"]}>HTML</li>
-              <li className={styles["stack-chip"]}>React</li>
-            </ul>
+            <li className={styles["stack-chip"]}>CSS</li>
+            <li className={styles["stack-chip"]}>TypeScript</li>
+            <li className={styles["stack-chip"]}>JavaScript</li>
+            <li className={styles["stack-chip"]}>Git</li>
+            <li className={styles["stack-chip"]}>HTML</li>
+            <li className={styles["stack-chip"]}>React</li>
+          </ul>
         </article>
       </div>
 
